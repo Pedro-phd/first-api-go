ba4f4e107272e6a77a324e64f99346306a9e7a7f
Pedro-phd <pedro_dominici@outlook.com>
2023-12-10 17:28:13 -0300

    feat: validation for fields

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,26 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/pedro-phd/first-api-go/src/configuration/rest_err"
+	"github.com/pedro-phd/first-api-go/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+
+	var userRequest request.UserResquest
+
+	// shouldBindJSON create a object from body of request,
+	// if validation of fields it is ok (binding in request struct)
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("some fields are incorrect, error: %s", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
+
+}
---
+++ src/controller/model/request/user_request.go
@@ -0,0 +1,10 @@
+package request
+
+//binding property for using with gin
+
+type UserResquest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8,containsany=!@#%$_."`
+	Name     string `json:"name" binding:"required,min=3,max=100"`
+	Age      int8   `json:"age" binding:"required,min=1,max=140"`
+}
